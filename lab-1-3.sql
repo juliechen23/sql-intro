cd6eff9812edc153c655c6b7d1d7a4ac2debe5d3
juliechen23 <julie.chen@kellogg.northwestern.edu>
2023-01-10 22:23:01 +0000

    i'm a sql pro after just 3 hours

--- lab-1-3.sql
+++ lab-1-3.sql
@@ -1,5 +1,5 @@
 -- Which three teams had the losingest seasons?
-
+SELECT year,name,wins, losses FROM teams ORDER BY losses DESC LIMIT 3
 -- Expected result:
 --
 -- +------+----------------------+------+--------+
